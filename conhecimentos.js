37e350b3ece4ce8c1cb74101450f4394e8b1e485
Ramiskelvin <140659191+Ramiskelvin@users.noreply.github.com>
2024-01-11 17:34:58 -0300

    the end

--- conhecimentos.js
+++ conhecimentos.js
@@ -1,7 +1,7 @@
 const lista = [
     'html o esqueleto do site é a parte que servira de base para todo o resto',
-    'css o estilo ou a pele do site basicamente a parte mais estetica do site',
-    'java animações'
+    'css é uma linguagem chamada de Cascading Style Sheet usado para estilizar html',
+    'javascript é uma linguagem de programação que permite implementar interatividade no site'
 ]
 function EventoTeste(index){
     const texto = document.querySelector('.paragrafo-conhecimento')
